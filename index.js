0dd612b9a5c31b4ae1bc7cf001068d0da24c7b50
Ala Taupeka <121459925+ataupeka@users.noreply.github.com>
2023-08-22 18:23:44 -0400

    Update index.js
    part2

--- index.js
+++ index.js
@@ -66,3 +66,64 @@ function validateInput(title, content){
     }, 500);
 
 }
+
+
+//create a new note div
+function createNote(noteItem){
+    const div = document.createElement('div');
+    div.classList.add('note-item');
+    div.setAttribute('data-id', noteItem.id);
+    div.innerHTML = `
+    <h3> ${noteItem.title}</h3>
+    <p>${noteItem.content}</p>
+
+    <button type="button" class=" btn btn-delete"> 
+        <span> <i class="fas fa-trash"></i> Remove</span>
+
+    </button>
+    `;
+    noteListDiv.appendChild(div);
+}
+
+//display all notes
+function displayNotes(){
+    let notes = getDataFromStorage();
+    if(notes.length > 0){
+        noteID = notes[notes.length -1].id;
+        noteID++;
+    } else {
+        noteID = 1;
+    } 
+    notes.forEach(item => {
+        createNote(item)
+    });
+}
+
+//delete 
+function deleteNoteItem(e){
+    if(e.target.classList.contains('btn-delete')){
+        e.target.parentElement.remove();//removing from DOM
+        let divID = e.target.parentElement.dataset.id;
+        let notes = getDataFromStorage();
+        let newNotesList = notes.filter(item => {
+            return item.id != parseInt(divID);
+        });
+        localStorage.setItem('notes', JSON.stringify(newNotesList));
+    }
+
+}
+
+// delete all
+function deleteAllNotes(){
+    localStorage.removeItem('notes');
+    let noteList = document.querySelectorAll('.note-item');
+    if(noteList.length > 0){
+        noteList.forEach(item => {
+            noteListDiv.removeChild(item);
+        });
+    }
+    noteID = 1;//resetting noteID to 1
+}
+
+
+
